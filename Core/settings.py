3d7b298e82d35dc1c06e2f934ccbd333811db18c
TEST <test@TESTs-MacBook-Pro.local>
2024-09-17 13:43:55 +0100

    added whitenoise middleware

--- Core/settings.py
+++ Core/settings.py
@@ -101,6 +101,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Core.urls'
