e4267bf6588a006f1d98fefcd26ecdb13d9ceb02
TEST <test@TESTs-MacBook-Pro.local>
2024-09-26 20:54:04 +0100

    styled feed page and post project modal

--- feed/forms.py
+++ feed/forms.py
@@ -6,3 +6,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['project_title', 'project_image', 'additional_notes', 'live_link', 'github_link']
+        widgets = {
+            'project_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter project title'}),
+            'additional_notes': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter project description'}),
+            'project_image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'live_link': forms.URLInput(attrs={'class': 'form-control', 'placeholder': 'Enter live project link'}),
+            'github_link': forms.URLInput(attrs={'class': 'form-control', 'placeholder': 'Enter GitHub link'}),
+        }
+
